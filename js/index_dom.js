a8154b89545661e670c9329b7d6d5ed7b737d418
cmonsalt <cristiantm12.com>
2021-12-06 21:40:37 -0500

    Se implemento reloj digital y alarma

---
+++ js/index_dom.js
@@ -0,0 +1,10 @@
+import hamburguerMenu from "./menu_hamburguesa.js";
+import { digitalClock, alarm } from "./reloj.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburguerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("assets/alarm-clock.mp3", "#activar-alarma", "#desactivar-alarma");
+});
