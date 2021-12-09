37d901ece45b80ac9b3ac95a6a7522de13f81823
cmonsalt <cristiantm12.com>
2021-12-08 21:31:45 -0500

    Seccion de colisiones

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,5 +1,6 @@
 import hamburguerMenu from "./menu_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
+import { moveBall } from "./teclado.js";
 
 const d = document;
 
@@ -7,4 +8,8 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarm-clock.mp3", "#activar-alarma", "#desactivar-alarma");
+});
+
+d.addEventListener("keydown", (e) => {
+  moveBall(e, ".ball", ".stage");
 });
---
+++ js/teclado.js
@@ -0,0 +1,39 @@
+const d = document;
+let x = 0,
+  y = 0;
+export function moveBall(e, ball, stage) {
+  const $ball = d.querySelector(ball),
+    $stage = d.querySelector(stage),
+    limitsBall = $ball.getBoundingClientRect(),
+    limitsStage = $stage.getBoundingClientRect();
+
+  switch (e.keyCode) {
+    case 37:
+      if (limitsBall.left > limitsStage.left) {
+        x--;
+        e.preventDefault();
+      }
+      break;
+    case 38:
+      if (limitsBall.top > limitsStage.top) {
+        y--;
+        e.preventDefault();
+      }
+      break;
+    case 39:
+      if (limitsBall.right < limitsStage.right) {
+        x++;
+        e.preventDefault();
+      }
+      break;
+    case 40:
+      if (limitsBall.bottom < limitsStage.bottom) {
+        y++;
+        e.preventDefault();
+      }
+      break;
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x * 10}px,${y * 10}px)`;
+}
